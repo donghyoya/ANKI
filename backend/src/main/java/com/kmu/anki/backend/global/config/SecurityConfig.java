c19ef6076bc1010369a397c449cbc190f5ec7ca3
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-17 16:25:31 +0900

    disable spring security for dev

---
+++ backend/src/main/java/com/kmu/anki/backend/global/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kmu.anki.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    @Profile("dev") // dev 프로파일에서만 적용
+    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}

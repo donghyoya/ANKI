921b2adc7c77eb11528137c671e1a4e7d3154759
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-18 14:06:06 +0900

    user entity

---
+++ backend/src/main/java/com/kmu/anki/backend/domain/user/entity/User.java
@@ -0,0 +1,13 @@
+package com.kmu.anki.backend.domain.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+}

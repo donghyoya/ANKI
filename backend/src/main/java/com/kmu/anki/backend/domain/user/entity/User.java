430f2fcfe33b1ab7298d14a16695f007ae1a5957
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-24 11:54:47 +0900

    feat: user-option-controller

--- backend/src/main/java/com/kmu/anki/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/kmu/anki/backend/domain/user/entity/User.java
@@ -1,15 +1,21 @@
 package com.kmu.anki.backend.domain.user.entity;
 
+import com.kmu.anki.backend.domain.card.enums.LanguageCode;
 import com.kmu.anki.backend.domain.usercard.entity.UserCard;
 import com.kmu.anki.backend.domain.usercard.entity.UserDeck;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -21,6 +27,15 @@ public class User {
     @Column
     private Integer todayReviewWords;
 
+    @Column
+    private LanguageCode languageCode;
+
+    public void update(Integer todayStudyWords, Integer todayReviewWords,LanguageCode languageCode){
+        this.todayStudyWords = todayStudyWords;
+        this.todayReviewWords = todayReviewWords;
+        this.languageCode = languageCode;
+    }
+
     /* 관계 : 유저덱 */
     @OneToMany(mappedBy = "user")
     private List<UserDeck> decks = new ArrayList<>();
@@ -29,7 +44,15 @@ public class User {
         decks.add(deck);
     }
 
+    /* 생성 */
 
+    public User(Integer todayStudyWords, Integer todayReviewWords) {
+        this(todayStudyWords, todayReviewWords, LanguageCode.en);
+    }
 
-
+    public User(Integer todayStudyWords, Integer todayReviewWords, LanguageCode languageCode) {
+        this.todayStudyWords = todayStudyWords;
+        this.todayReviewWords = todayReviewWords;
+        this.languageCode = languageCode;
+    }
 }

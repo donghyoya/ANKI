23fc08969790a66325390f1cfa29e52d2969d508
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-01-04 21:07:24 +0900

    test: disable localFileSystemAccessObjectTest

--- backend/src/test/java/com/kmu/anki/backend/file/access/impl/LocalFileSystemAccessObjectTest.java
+++ backend/src/test/java/com/kmu/anki/backend/file/access/impl/LocalFileSystemAccessObjectTest.java
@@ -1,10 +1,12 @@
 package com.kmu.anki.backend.file.access.impl;
 
 import com.kmu.anki.backend.file.access.FileSystemAccessObject;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 class LocalFileSystemAccessObjectTest {
     private final FileSystemAccessObject fileSAO = new LocalFileSystemAccessObject();
 

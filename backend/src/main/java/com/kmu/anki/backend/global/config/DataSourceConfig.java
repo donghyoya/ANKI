fa7bad8b22da86ae3734327400d8813a39488b23
donghyoya <dongwooya123@gmail.com>
2025-01-11 21:36:30 +0900

    setting swagger to prod 4

---
+++ backend/src/main/java/com/kmu/anki/backend/global/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.kmu.anki.backend.global.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url:NOT SET}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username:NOT SET}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password:NOT SET}")
+    private String datasourcePassword;
+
+    @Value("${spring.datasource.driver-class-name:NOT SET}")
+    private String driverClassName;
+
+    @PostConstruct
+    public void logDataSourceProperties() {
+        System.out.println("=======================================");
+        System.out.println("Spring Datasource Configuration:");
+        System.out.println("URL: " + datasourceUrl);
+        System.out.println("Username: " + datasourceUsername);
+        System.out.println("Password: " + datasourcePassword);
+        System.out.println("Driver Class Name: " + driverClassName);
+        System.out.println("=======================================");
+    }
+}

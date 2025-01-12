d3c10f5c6b0d6cbbef425bd94dfe4d16faab8ce7
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-01-12 16:01:57 +0900

    docs: request parameters enum

--- backend/src/test/java/com/kmu/anki/backend/domain/card/docs/parameters/DeckParameters.java
+++ backend/src/test/java/com/kmu/anki/backend/domain/card/docs/parameters/DeckParameters.java
@@ -1,18 +1,30 @@
 package com.kmu.anki.backend.domain.card.docs.parameters;
 
 import com.kmu.anki.backend.domain.card.controller.QueryType;
+import com.kmu.anki.backend.domain.card.enums.CardLevel;
+import com.kmu.anki.backend.domain.card.enums.CardTopicEnums;
 import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.restdocs.snippet.Attributes;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 
 public class DeckParameters {
     public static ParameterDescriptor queryType = parameterWithName("queryType")
-            .description("의미에 따른 분류인가 (level) / 난이도에 따른 분류인가 (meaning)");
+            .description("난이도 기준 검색인지, 의미상 구분에 따른 검색인지")
+            .attributes(Attributes.key("enumValues").value(Arrays.asList(QueryType.values())));
 
-    public static ParameterDescriptor query = parameterWithName("query").description("검색어 (easy-normal-hard 등)");
+    public static ParameterDescriptor query = parameterWithName("query")
+            .description("검색어 (easy-normal-hard 등)")
+            .attributes(Attributes.key("enumValues").value(queryEnums()))
+    ;
+
+    private static List<Object> queryEnums(){
+        List<Object> values = new ArrayList<>();
+        values.addAll(Arrays.asList(CardLevel.values()));
+        values.addAll(Arrays.asList(CardTopicEnums.values()));
+        return values;
+    }
 }
--- backend/src/test/java/com/kmu/anki/backend/domain/usercard/docs/parameters/UserCardParameters.java
+++ backend/src/test/java/com/kmu/anki/backend/domain/usercard/docs/parameters/UserCardParameters.java
@@ -1,11 +1,19 @@
 package com.kmu.anki.backend.domain.usercard.docs.parameters;
 
+import com.kmu.anki.backend.domain.usercard.controller.form.StudyType;
 import org.springframework.restdocs.request.ParameterDescriptor;
+import org.springframework.restdocs.snippet.Attributes;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
+
+import java.util.Arrays;
 
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 
 public class UserCardParameters {
     public static ParameterDescriptor cardId = parameterWithName("id").description("card 고유번호");
 
-    public static ParameterDescriptor studyType = parameterWithName("studyType").description("study냐 review냐");
+    public static ParameterDescriptor studyType = parameterWithName("studyType")
+            .description("study냐 review냐")
+            .attributes(Attributes.key("enumValues").value(Arrays.asList(StudyType.values())))
+        ;
 }

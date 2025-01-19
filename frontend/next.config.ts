f962884a9b351234edc895ac27956eaaa3505eff
nanana3679 <nanana3679@gmail.com>
2025-01-20 03:44:30 +0900

    chore: postcss config
    - install autoprefixer, postcss-pxtorem
    - add .browserslist, postcss.config.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  postcss: {
+    plugins: {
+      'postcss-pxtorem': {
+        rootValue: 16,
+        propList: ['*']
+      },
+      autoprefixer: {}
+    }
+  }
 };
 
 export default nextConfig;

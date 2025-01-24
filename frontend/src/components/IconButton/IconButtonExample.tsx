1952c107723c1d90146d1c92b7e6cd7d4960cd82
nanana3679 <nanana3679@gmail.com>
2025-01-25 00:59:18 +0900

    feat: add Icon variations

--- frontend/src/components/IconButton/IconButtonExample.tsx
+++ frontend/src/components/IconButton/IconButtonExample.tsx
@@ -13,6 +13,15 @@ export const IconButtonExample = () => {
   return (
     <>
       <Icon className={styles['icon']}>check</Icon>
+
+      {/* fontVariationSettings에 대한 설명: https://developers.google.com/fonts/docs/material_symbols?hl=ko#fill_axis */}
+      <div style={{ fontVariationSettings: "'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;" }}>
+        <Icon>search</Icon>
+        <Icon>home</Icon>
+        <Icon>settings</Icon>
+        <Icon>favorite</Icon>
+      </div>
+
       <IconButton className={styles['icon-button']}>
         <Icon>check</Icon>
       </IconButton>

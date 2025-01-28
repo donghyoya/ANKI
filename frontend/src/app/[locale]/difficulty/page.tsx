c39b7203b6f7d04042f7d9dca4eaf8ba1fa4726c
cherry <jms02140@gmail.com>
2025-01-28 14:57:27 +0900

    feat: add difficulty page

--- frontend/src/app/[locale]/difficulty/page.tsx
+++ frontend/src/app/[locale]/difficulty/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import DeckCard from '@/components/DeckCard/DeckCard';
 import { useTranslations } from 'next-intl';
+import styles from './Difficulty.module.scss'
 
 export default function DifficultyPage() {
   const t = useTranslations();
@@ -15,14 +16,41 @@ export default function DifficultyPage() {
     alert('Starting learning!');
   };
 
+  const buttonLabels = {
+    viewWords: t('viewWords'),
+    learn: t('learn'),
+  }
+
   return (
-    <div>
-      <DeckCard
-        title={t('beginner')}
-        wordCount={1234}
-        onViewWords={handleViewWords}
-        onLearn={handleLearn}
-      />
+    <div className={styles.page}>
+      <div className={styles.content}>
+        <div className={`${styles.title} md-typescale-headline-large`}>
+          Words List
+        </div>
+        <div className={styles.cards}>
+          <DeckCard
+            title={t('beginner')}
+            wordCount={1234}
+            buttonLabels={buttonLabels}
+            onViewWords={handleViewWords}
+            onLearn={handleLearn}
+          />
+          <DeckCard
+            title={t('intermediate')}
+            wordCount={1234}
+            buttonLabels={buttonLabels}
+            onViewWords={handleViewWords}
+            onLearn={handleLearn}
+          />
+          <DeckCard
+            title={t('advanced')}
+            wordCount={1234}
+            buttonLabels={buttonLabels}
+            onViewWords={handleViewWords}
+            onLearn={handleLearn}
+          />
+        </div>
+      </div>
     </div>
   );
 }

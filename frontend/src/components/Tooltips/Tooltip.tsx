27c8bcdc71bb201edc67a74edc3260062cef7b0a
nanana3679 <nanana3679@gmail.com>
2025-02-02 17:48:06 +0900

    fix: pass extraGap prop to Tooltip component

--- frontend/src/components/Tooltips/Tooltip.tsx
+++ frontend/src/components/Tooltips/Tooltip.tsx
@@ -1,10 +1,11 @@
 import styles from './Tooltip.module.scss';
 
-const Tooltip = ({ text, rect }: { text: string; rect: DOMRect }) => {
+const Tooltip = ({ text, rect, extraGap }: { text: string; rect: DOMRect; extraGap?: boolean }) => {
   return (
     <div
       className={styles['tooltip']}
       data-tooltip={text}
+      {...(extraGap && { 'data-extra-gap': true })}
       style={{
         top: rect.top,
         left: rect.left,

f3f4862af42c2f66e98a57428c8cdbc4e65c63b5
cherry <jms02140@gmail.com>
2025-02-03 16:02:20 +0900

    fix: modify locales (delete 'ms', add 'id')

--- frontend/src/i18n/routing.ts
+++ frontend/src/i18n/routing.ts
@@ -1,15 +1,15 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['ar', 'en', 'es', 'fr', 'ja', 'ko', 'mn', 'ms', 'ru', 'th', 'vi', 'zh'],
- 
+  locales: ['ar', 'en', 'es', 'fr', 'id', 'ja', 'ko', 'mn', 'ru', 'th', 'vi', 'zh'],
+
   // Used when no locale matches
   defaultLocale: 'en'
 });
- 
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);

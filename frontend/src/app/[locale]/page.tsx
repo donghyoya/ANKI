973d59bb5d17020a0a21e1e820ab2eba13fae7e6
nanana3679 <nanana3679@gmail.com>
2025-02-07 07:02:58 +0900

    feat: add M3 grid system

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,41 +1,41 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
-import { Link } from '@/i18n/routing';
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
-
-const FilledButton = dynamic(() => import('@/components/material-components/FilledButton'), {
-  ssr: false
-});
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
-  const t = useTranslations('HomePage');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const GridItem = ({ color }: { color: string }) => (
+    <div
+      className="grid-item"
+      style={{ backgroundColor: color, gridColumn: 'span 1', opacity: 0.5 }}
+    />
+  );
+
+  const GridItems = () => (
+    <>
+      <GridItem color="red" />
+      <GridItem color="blue" />
+      <GridItem color="green" />
+      <GridItem color="yellow" />
+    </>
+  );
+
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <Link href="/about">{t('about')}</Link>
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <FilledButton>Hello</FilledButton>
-          {/* examples */}
-          <p className="md-typescale-display-large">Display Large</p>
-          <p className="md-typescale-display-medium">Display Medium</p>
-          <p className="md-typescale-display-small">Display Small</p>
-          <p className="md-typescale-headline-large">Headline Large</p>
-          <p className="md-typescale-headline-medium">Headline Medium</p>
-          <p className="md-typescale-headline-small">Headline Small</p>
-          <h1 className="md-typescale-title-large">Title Large</h1>
-          <h2 className="md-typescale-title-medium">Title Medium</h2>
-          <h3 className="md-typescale-title-small">Title Small</h3>
-          <p className="md-typescale-body-large">Body Large</p>
-          <p className="md-typescale-body-medium">Body Medium</p>
-          <p className="md-typescale-body-small">Body Small</p>
-          <p className="md-typescale-label-large">Label Large</p>
-          <p className="md-typescale-label-medium">Label Medium</p>
-          <p className="md-typescale-label-small">Label Small</p>
-        </Suspense>
-      </div>
+    <div className="grid-container">
+      <GridItems />
+      {width > 600 && <GridItems />}
+      {width > 840 && <GridItems />}
     </div>
   );
 }

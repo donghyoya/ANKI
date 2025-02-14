edd48df16ed85dec36701a9f888efe9fc5e710f1
nanana3679 <nanana3679@gmail.com>
2025-02-14 16:30:57 +0900

    feat: add responsive typography to WordSection

--- frontend/src/components/LearningCard/WordSection.tsx
+++ frontend/src/components/LearningCard/WordSection.tsx
@@ -7,30 +7,27 @@ interface WordSectionProps {
 }
 
 const WordSection = ({ wordInfo }: WordSectionProps) => {
+  const levelLabel =
+    wordInfo.level === 'beginner' ? '★★★' : 'intermediate' ? '★★' : 'advanced' ? '★' : '';
+
   return (
     <>
       <div className={styles['korean-container']}>
-        <span className="md-typescale-headline-large">{wordInfo.koreanWord}</span>
-        <span className="md-typescale-headline-small">{wordInfo.homographNumber}</span>
+        <span className={styles['korean-word']}>{wordInfo.koreanWord}</span>
+        <span className={styles['korean-homograph-number']}>{wordInfo.homographNumber}</span>
         <div className={styles['korean-info-container']}>
-          <span className="md-typescale-label-small">{wordInfo.level}</span>
+          <span className={styles['korean-level']}>{levelLabel}</span>
           <div className={styles['korean-info-sub-container']}>
-            <span className={`${styles['pronunciation']} md-typescale-label-large`}>
-              {`[${wordInfo.pronunciation}]`}
-            </span>
-            <span className={`${styles['origin']} md-typescale-label-large`}>
-              {wordInfo.originalLanguage ?? ''}
-            </span>
+            <span className={styles['pronunciation']}>{`[${wordInfo.pronunciation}]`}</span>
+            <span className={styles['origin']}>{wordInfo.originalLanguage ?? ''}</span>
           </div>
         </div>
       </div>
       <div className={styles['foreign-container']}>
-        <span className={`${styles['foreign-word']} md-typescale-body-large`}>
+        <span className={styles['foreign-word']}>
           1. {wordInfo.partsOfSpeech} {wordInfo.foreignWord}
         </span>
-        <span className={`${styles['foreign-word-sub']} md-typescale-label-medium`}>
-          {wordInfo.relatedWords}
-        </span>
+        <span className={styles['foreign-word-sub']}>{wordInfo.relatedWords}</span>
       </div>
     </>
   );

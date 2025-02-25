978fc048681a27a0d0f321af7b63e2b0f8c305ac
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-25 20:17:42 +0900

    feat: logging
    Exception 핸들러에서 로그를 남기지 않는 문제 해결

--- backend/src/main/java/com/kmu/anki/backend/global/controller/GlobalExceptionController.java
+++ backend/src/main/java/com/kmu/anki/backend/global/controller/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package com.kmu.anki.backend.global.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,20 +11,25 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionController {
+
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ExceptionResponse> handleNotFound(NoSuchElementException ex) {
+        log.error("[404] NoSuchElementException: {}", ex.getMessage(), ex);
         return new ResponseEntity<>(ExceptionResponse.of(404, "NOT FOUND"), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class, MethodArgumentTypeMismatchException.class})
     public ResponseEntity<ExceptionResponse> handleBadRequest(RuntimeException ex){
+        log.error("[400] MethodArgumentNotValidException: {}", ex.getMessage(), ex);
         return new ResponseEntity<>(ExceptionResponse.of(400, "query parameter is invalid"), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ExceptionResponse> handleRunTimeException(RuntimeException ex){
+        log.error("[500] RuntimeException: {}", ex.getMessage(), ex);
         return new ResponseEntity<>(ExceptionResponse.of(500, "INTERNAL_SERVER_ERROR"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 

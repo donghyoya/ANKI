31c6bc518eac824cab09ee1b8a490b998bfca03f
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-25 20:27:52 +0900

    fix: mybatis
    mybatis의 xml mapper를 등록하지 않아서 생긴 문제 해결

--- backend/src/main/java/com/kmu/anki/backend/domain/usercard/repository/UserCardMapper.java
+++ backend/src/main/java/com/kmu/anki/backend/domain/usercard/repository/UserCardMapper.java
@@ -1,8 +1,9 @@
 package com.kmu.anki.backend.domain.usercard.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserCardMapper {
-    void studyDeck(Long userId);
+    void studyDeck(@Param("userId") Long userId);
 }

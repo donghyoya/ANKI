0324616bc598966d192446b6bfe1c4563556307f
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-02-26 15:43:01 +0900

    fix: cors

--- backend/src/main/java/com/kmu/anki/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/kmu/anki/backend/global/config/SecurityConfig.java
@@ -16,6 +16,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -43,7 +49,7 @@ public class SecurityConfig {
         http
                 // 0. CSRF 비활성화 (필요 시)
                 .csrf(csrf -> csrf.disable())
-                .cors(cors->cors.disable())
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
@@ -79,6 +85,20 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID")// 쿠키 삭제
                 );
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean

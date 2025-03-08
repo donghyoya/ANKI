da95c2a51fb66f5bcdf706b9cb0d0e929fa316d9
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-08 15:35:56 +0900

    test: study usercard repeat

---
+++ backend/src/test/java/com/kmu/anki/backend/domain/usercard/service/UserCardServiceTest.java
@@ -0,0 +1,36 @@
+package com.kmu.anki.backend.domain.usercard.service;
+
+import com.kmu.anki.backend.domain.card.enums.CardLevel;
+import com.kmu.anki.backend.domain.card.enums.LanguageCode;
+import com.kmu.anki.backend.domain.user.entity.CardState;
+import com.kmu.anki.backend.domain.usercard.dto.UserCardDto;
+import com.kmu.anki.backend.global.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserCardServiceTest extends AbstractIntegrationTest {
+    @Autowired
+    private UserCardService userCardService;
+
+    @Test
+    void readStudyUserCardRepeat() {
+        Long userId = 1L;
+        LanguageCode code = LanguageCode.en;
+        CardLevel level = CardLevel.easy;
+
+        Page<UserCardDto> userCardDtos = userCardService.readStudyUserCard(userId, code, level);
+        UserCardDto userCardDto = userCardDtos.getContent().get(0);
+
+        userCardService.updateUserCard(userCardDto.getUserCardId(), LocalDateTime.now(), 1, LocalDateTime.now(), 1, 0.1d, 0.1d, CardState.Review);
+
+        Page<UserCardDto> repeatUserCardDtos = userCardService.readStudyUserCard(userId, code, level);
+
+        assertEquals(userCardDtos, repeatUserCardDtos);
+    }
+
+}

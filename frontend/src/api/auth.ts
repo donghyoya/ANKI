85140e5079c66dd02b33aafa5cb1c4cbc463a9ab
nanana3679 <nanana3679@gmail.com>
2025-04-13 17:15:08 +0900

    chore: Remove deprecated login APIs

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,81 +1,8 @@
-const endpoint = process.env.NEXT_PUBLIC_API_URL;
+export function getGoogleLoginLink() {
+  const server = process.env.NEXT_PUBLIC_SERVER;
+  const endpoint = process.env.NEXT_PUBLIC_AUTH_ENDPOINT;
+  const redirect = process.env.NEXT_PUBLIC_REDIRECT_URI;
+  const myhost = process.env.NEXT_PUBLIC_MY_HOST;
 
-export const login = async () => {
-  const url = `/api/auth/oauth2/google`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-};
-
-export const handleLogin = () => {
-  const width = 500;
-  const height = 600;
-  const left = window.screenX + (window.outerWidth - width) / 2;
-  const top = window.screenY + (window.outerHeight - height) / 2;
-
-  return new Promise((resolve) => {
-    window.open(
-      `${endpoint}/oauth2/authorization/google`,
-      'Google 로그인',
-      `width=${width},height=${height},left=${left},top=${top}`
-    );
-
-    window.addEventListener(
-      'message',
-      function (event) {
-        resolve(event.data);
-      },
-      { once: true }
-    );
-  });
-};
-
-export const checkLogin = async () => {
-  const url = `${endpoint}/api/auth/checkLogin.do`;
-  const response = await fetch(url, {
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    }
-  });
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-
-export const logout = async () => {
-  const url = `${endpoint}/api/auth/logout.do`;
-  const response = await fetch(url, {
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
-    }
-  });
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-
-// 토큰 기반 로그인 구현
-export const tokenLoginTest = async (token: string) => {
-  if (!token) return;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/auth-test`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    }
-  });
-
-  if (res.status === 401) {
-    throw new Error('로그인이 필요합니다.');
-  }
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch');
-  }
-
-  const data = await res.json();
-  return data;
-};
+  return `${server}/${endpoint}?redirect_uri=${encodeURIComponent(`${myhost}/${redirect}`)}`;
+}
--- frontend/src/app/[locale]/login/page.tsx
+++ frontend/src/app/[locale]/login/page.tsx
@@ -1,37 +1,15 @@
 'use client';
 
-function getGoogleLoginLink() {
-  const server = process.env.NEXT_PUBLIC_SERVER;
-  const endpoint = process.env.NEXT_PUBLIC_AUTH_ENDPOINT;
-  const redirect = process.env.NEXT_PUBLIC_REDIRECT_URI;
-  const myhost = process.env.NEXT_PUBLIC_MY_HOST;
+import { getGoogleLoginLink } from '@/api/auth';
 
-  return `${server}/${endpoint}?redirect_uri=${encodeURIComponent(`${myhost}/${redirect}`)}`;
-}
+import Link from 'next/link';
 
 export default function LoginPage() {
   const link = getGoogleLoginLink();
 
   return (
     <div>
-      <h1>로그인 페이지</h1>
-
-      <h3>
-        <a href={link}>
-          {/* #중요# */}
-          구글 로그인
-        </a>
-      </h3>
-
-      <p>{link}</p>
-
-      <p>
-        이 링크로 접근하게 되면 소셜 로그인 절차가 <strong>spring oauth2-client</strong>
-        라이브러리에 의해서 수행되게 됩니다. 일련의 로그인 절차를 수행한 이후
-        <code>/auth/redirect</code>로 다시 되돌아오게 됩니다.
-        <br />
-        현재는 <code>localhost:3000/auth/redirect</code>만 수행됩니다.
-      </p>
+      <Link href={link}>구글 로그인</Link>
     </div>
   );
 }

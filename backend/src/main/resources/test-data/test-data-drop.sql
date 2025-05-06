b4f04d93cfb3594a3830f9bcdd0493bf076d08b0
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-05-06 15:28:00 +0900

    test: test data 삽입 및 삭제 처리

--- backend/src/main/resources/test-data/test-data-drop.sql
+++ backend/src/main/resources/test-data/test-data-drop.sql
@@ -1,6 +1,8 @@
 truncate user_cards cascade;
 truncate users cascade;
 truncate foreign_cards cascade;
+truncate korean_meanings cascade;
 truncate korean_cards cascade;
 -- truncate topics cascade;
 truncate card_topics cascade;
+truncate topics cascade;

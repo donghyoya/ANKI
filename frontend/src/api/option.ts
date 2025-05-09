7fb202bfae7b9668f9a0c7d0361e14c9b128e0dd
nanana3679 <nanana3679@gmail.com>
2025-05-10 05:32:18 +0900

    refactor: API 요청 유틸리티 구현 및 API 호출 리팩토링

--- frontend/src/api/option.ts
+++ frontend/src/api/option.ts
@@ -1,44 +1,18 @@
 'use server';
 
 import { UserOption } from '@/types/schemes';
+import { apiRequest } from './utils';
 
 const endpoint = process.env.NEXT_PUBLIC_SERVER;
 
 export const getUserOption = async (token: string) => {
   const url = `${endpoint}/user/option`;
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    }
-  });
-
-  const data = await response.json();
-
-  if (response.ok) {
-    return data as UserOption;
-  } else {
-    throw new Error(data.message);
-  }
+  const response = await apiRequest<UserOption>({ url, token });
+  return response;
 };
 
 export const postUserOption = async (userOption: UserOption, token: string) => {
   const url = `${endpoint}/user/option`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userOption)
-  });
-
-  const data = await response.json();
-
-  if (response.ok) {
-    return data as UserOption;
-  } else {
-    throw new Error(data.message);
-  }
+  const response = await apiRequest<UserOption>({ url, token, method: 'POST', body: userOption });
+  return response;
 };

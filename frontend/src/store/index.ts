f346f45a6d76ee227e827dadd4671e324c765c84
nanana3679 <nanana3679@gmail.com>
2025-05-15 18:14:35 +0900

    chore: redux-devtool 설정
    - 개발 환경에서만 활성화

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,7 +6,8 @@ export const store = configureStore({
   reducer: {
     studyQueue: studyQueueReducer,
     auth: authReducer
-  }
+  },
+  devTools: process.env.NODE_ENV !== 'production'
 });
 
 export type RootState = ReturnType<typeof store.getState>;

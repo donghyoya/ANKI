9ed34b554eabe0f9475e3a52d55633ca6e05955e
nanana3679 <nanana3679@gmail.com>
2025-06-02 22:17:51 +0900

    feat: 학습 진행 상태에 'Overdue' 상태 추가

--- frontend/src/components/ProgressBar/LearningProgressBar.tsx
+++ frontend/src/components/ProgressBar/LearningProgressBar.tsx
@@ -5,44 +5,46 @@ import styles from './LearningProgressBar.module.scss';
 import { UserCard } from '@/types/schemes';
 import { Progress } from '@/types/Progress';
 import { LEARNING_PROGRESS_BAR_COLORS } from '@/constants/colors';
-import { useEffect, useMemo } from 'react';
 import { State } from 'ts-fsrs';
 
 const LearningProgressBar = ({ userCards }: { userCards: UserCard[]; className: string }) => {
-  const progress: Progress[] = useMemo(
-    () => [
-      {
-        value: userCards.filter((card) => card.studyInfo.state === State.Review).length || 0,
-        label: userCards.filter((card) => card.studyInfo.state === State.Review).length,
-        tooltip: 'Matured',
-        color: LEARNING_PROGRESS_BAR_COLORS.Review
-      },
-      {
-        value:
-          userCards.filter(
-            (card) =>
-              card.studyInfo.state === State.Learning || card.studyInfo.state === State.Relearning
-          ).length || 0,
-        label: userCards.filter(
-          (card) =>
-            card.studyInfo.state === State.Learning || card.studyInfo.state === State.Relearning
-        ).length,
-        tooltip: 'Learning',
-        color: LEARNING_PROGRESS_BAR_COLORS.Learning
-      },
-      {
-        value: userCards.filter((card) => card.studyInfo.state === State.New).length || 0,
-        label: userCards.filter((card) => card.studyInfo.state === State.New).length,
-        tooltip: 'New',
-        color: LEARNING_PROGRESS_BAR_COLORS.New
-      }
-    ],
-    [userCards]
-  );
+  const reviewCounts = userCards.filter(
+    (card) => card.studyInfo.state === State.Review && card.studyInfo.due >= new Date()
+  ).length;
+  const learningCounts = userCards.filter(
+    (card) => card.studyInfo.state === State.Learning || card.studyInfo.state === State.Relearning
+  ).length;
+  const overdueCounts = userCards.filter(
+    (card) => card.studyInfo.state === State.Review && card.studyInfo.due < new Date()
+  ).length;
+  const newCounts = userCards.filter((card) => card.studyInfo.state === State.New).length;
 
-  useEffect(() => {
-    console.log(progress);
-  }, [progress]);
+  const progress: Progress[] = [
+    {
+      value: reviewCounts,
+      label: reviewCounts,
+      tooltip: 'Completed',
+      color: LEARNING_PROGRESS_BAR_COLORS.Review
+    },
+    {
+      value: overdueCounts,
+      label: overdueCounts,
+      tooltip: 'Review',
+      color: LEARNING_PROGRESS_BAR_COLORS.Overdue
+    },
+    {
+      value: newCounts,
+      label: newCounts,
+      tooltip: 'New',
+      color: LEARNING_PROGRESS_BAR_COLORS.New
+    },
+    {
+      value: learningCounts,
+      label: learningCounts,
+      tooltip: 'Learning',
+      color: LEARNING_PROGRESS_BAR_COLORS.Learning
+    }
+  ];
 
   return (
     <div className={styles['container']}>
--- frontend/src/constants/colors.ts
+++ frontend/src/constants/colors.ts
@@ -1,6 +1,7 @@
 export const LEARNING_PROGRESS_BAR_COLORS = {
-  Review: '#D0ECAF', // green
-  Learning: '#EDED80', // yellow
-  Relearning: '#FFB0B0', // red
-  New: '#D9D9D9' // gray
+  Review: '#B4EDC9', // mint
+  Overdue: '#D3E671', // green
+  New: '#F8ED8C', // yellow
+  Learning: '#FFB0B0', // red
+  Relearning: '#FFB0B0' // red
 };

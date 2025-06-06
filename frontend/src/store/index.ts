47128edfdb1c45a2e54bde2f4860939954092623
nanana3679 <nanana3679@gmail.com>
2025-06-07 03:47:59 +0900

    remove: 기존 인증 관련 파일 삭제

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import studyQueueReducer from './slices/studySlice';
-import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
-    studyQueue: studyQueueReducer,
-    auth: authReducer
+    studyQueue: studyQueueReducer
   },
   devTools: process.env.NODE_ENV !== 'production'
 });

08bb501ebc9b1fe8bfdc6cd2cce9cee52a610020
nanana3679 <nanana3679@gmail.com>
2025-06-11 17:45:46 +0900

    fix: CookieService를 /auth/redirect/page.ts 에서 호출하지 않도록 수정
    - getToken → refresh 쿠키에 토큰을 저장하도록 변경

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,8 +1,11 @@
+'use server';
+
 import { TokenDTO } from '@/types/schemes';
+import { ServerServiceFactory } from '@/services/ServerServiceFactory';
 
 const endpoint = process.env.NEXT_PUBLIC_SERVER;
 
-export const getToken = async (authenticationToken: string): Promise<TokenDTO> => {
+export const refresh = async (authenticationToken: string): Promise<TokenDTO> => {
   try {
     const url = `${endpoint}/auth/token`;
     console.log('authenticationToken', authenticationToken);
@@ -18,6 +21,9 @@ export const getToken = async (authenticationToken: string): Promise<TokenDTO> =
     const data = await response.json();
 
     if (response.ok) {
+      const cookieService = ServerServiceFactory.getCookieService();
+      await cookieService.set('accessToken', data.accessToken);
+      await cookieService.set('refreshToken', data.refreshToken);
       return data as TokenDTO;
     }
 

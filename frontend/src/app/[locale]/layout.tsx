3843e90efca870a89c4a1f83a3f2a23c3041034c
nanana3679 <nanana3679@gmail.com>
2025-06-20 21:52:20 +0900

    chore: QueryProvider 설정

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from '@/components/Navigation/Navigation';
 import I18nProvider from '@/providers/I18nProvider';
 import { StoreProvider } from '@/providers/StoreProvider';
 import ErrorBoundaryWrapper from '@/providers/ErrorBoundaryWrapper';
+import { QueryProvider } from '@/providers/QueryProvider';
 
 import styles from './layout.module.scss';
 
@@ -9,12 +10,14 @@ const NavigationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ErrorBoundaryWrapper>
       <StoreProvider>
-        <div className={styles['navigation-layout']}>
-          <I18nProvider>
-            <Navigation />
-            {children}
-          </I18nProvider>
-        </div>
+        <QueryProvider>
+          <div className={styles['navigation-layout']}>
+            <I18nProvider>
+              <Navigation />
+              {children}
+            </I18nProvider>
+          </div>
+        </QueryProvider>
       </StoreProvider>
     </ErrorBoundaryWrapper>
   );

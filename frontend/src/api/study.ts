941dfca28c822d8f04891fb2400579347f807ff4
nanana3679 <nanana3679@gmail.com>
2025-06-27 19:38:34 +0900

    fix: 누락된 studyInfo 변환 함수 추가

--- frontend/src/api/study.ts
+++ frontend/src/api/study.ts
@@ -2,7 +2,7 @@
 
 import { Paginated, StudyType, StudyInfo, StudyInfoDTO, UserCardDTO } from '@/types/schemes';
 import { Category, getCategoryType } from '@/types/Category';
-import { normalizeQuery, toUserCard } from '@/utils/converter';
+import { normalizeQuery, toStudyInfo, toUserCard } from '@/utils/converter';
 import { ServerServiceFactory } from '@/services/ServerServiceFactory';
 
 const httpClient = ServerServiceFactory.getHttpClient();
@@ -21,11 +21,11 @@ export const getLearningCards = async (studyType: StudyType, query: Category) =>
 export const getStudyInfo = async (cardId: number) => {
   const url = `/cards/${cardId}/study`;
   const response = await httpClient.get<StudyInfoDTO>(url);
-  return response.data;
+  return toStudyInfo(response.data);
 };
 
 export const postStudyInfo = async (userCardId: number, studyInfo: StudyInfo) => {
   const url = `/cards/${userCardId}/study`;
   const response = await httpClient.post<StudyInfoDTO>(url, studyInfo);
-  return response.data;
+  return toStudyInfo(response.data);
 };

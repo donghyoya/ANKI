eccf1a2956bb9b140fa70c2ff1212adef54c456a
cherry <jms02140@gmail.com>
2025-07-08 17:41:12 +0900

    feat: implement cookie consent notice

--- frontend/src/app/[locale]/difficulty/page.tsx
+++ frontend/src/app/[locale]/difficulty/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { useTranslations } from 'next-intl';
 
 import DeckListPage from '@/components/common/DeckListPage';
 import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
@@ -8,9 +9,12 @@ import { getDecks } from '@/api/decks';
 
 import { Deck } from '@/types/schemes';
 import { difficultiesInDisplayOrder } from '@/types/Category';
+import CustomDialog from '@/components/Dialogs/CustomDialog';
 
 export default function DifficultyPage() {
+  const t = useTranslations();
   const [decks, setDecks] = useState<Deck[]>();
+  const [isCookieConsentOpen, setIsCookieConsentOpen] = useState(false);
 
   useEffect(() => {
     const fetchUserCards = async () => {
@@ -20,17 +24,36 @@ export default function DifficultyPage() {
       }
     };
     fetchUserCards();
+
+    const cookieConsent = localStorage.getItem('cookieConsent');
+    if (!cookieConsent) {
+      setIsCookieConsentOpen(true);
+    }
   }, []);
+
+  const handleCookieConsent = () => {
+    localStorage.setItem('cookieConsent', 'true');
+    setIsCookieConsentOpen(false);
+  };
 
   if (!decks) {
     return <LoadingSpinner />;
   }
 
   return (
-    <DeckListPage
-      decks={decks}
-      categoryType="difficulty"
-      displayOrder={difficultiesInDisplayOrder}
-    />
+    <>
+      <DeckListPage
+        decks={decks}
+        categoryType="difficulty"
+        displayOrder={difficultiesInDisplayOrder}
+      />
+      <CustomDialog
+        open={isCookieConsentOpen}
+        headline={t('cookies')}
+        prompt={t('cookieConsent')}
+        firstButtonString={t('ok')}
+        firstButtonOnclick={handleCookieConsent}
+      />
+    </>
   );
 }

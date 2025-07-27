1ee17cc4538b10fe689be4c307e8fa01bec15d83
nanana3679 <nanana3679@gmail.com>
2025-07-27 11:33:05 +0900

    feat: 애니메이션 transition 정의

--- frontend/src/components/Navigation/NavigationRail.tsx
+++ frontend/src/components/Navigation/NavigationRail.tsx
@@ -7,7 +7,6 @@ import { motion } from 'framer-motion';
 
 import styles from './NavigationRail.module.scss';
 import { Icon, IconButton } from '@/components/material-components/IconButton/IconButton';
-// import { Ripple } from '@/components/material-components/Ripple';
 
 const NavigationRail = ({
   destinations,
@@ -51,6 +50,10 @@ const NavigationRail = ({
         className={styles['header-container']}
         initial={{ height: 64 }}
         animate={{ height: isExpanded ? 52 : 64 }}
+        transition={{
+          duration: 0.2,
+          ease: 'easeInOut'
+        }}
       >
         {isMenuEnabled && (
           <IconButton onClick={handleExpand}>
@@ -69,14 +72,22 @@ const NavigationRail = ({
               <motion.div
                 className={styles['navigation-button-background-container']}
                 animate={{
-                  height: isExpanded ? '56px' : '32px'
+                  height: isExpanded ? '56px' : '32px',
+                  transition: {
+                    duration: 0.2,
+                    ease: 'easeInOut'
+                  }
                 }}
               >
                 <motion.div
                   className={styles['navigation-button-background']}
                   initial={{ width: 0 }}
                   animate={{
-                    width: selectedDestination === destination.label ? '100%' : 0
+                    width: selectedDestination === destination.label ? '100%' : 0,
+                    transition: {
+                      duration: 0.2,
+                      ease: 'easeInOut'
+                    }
                   }}
                 />
               </motion.div>
@@ -85,7 +96,11 @@ const NavigationRail = ({
                 animate={{
                   fontVariationSettings:
                     selectedDestination === destination.label ? "'FILL' 1" : "'FILL' 0",
-                  top: isExpanded ? 16 : 4
+                  top: isExpanded ? 16 : 4,
+                  transition: {
+                    duration: 0.2,
+                    ease: 'easeInOut'
+                  }
                 }}
               >
                 <Icon>{destination.icon}</Icon>
@@ -95,6 +110,10 @@ const NavigationRail = ({
                   className={styles['navigation-item-label']}
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
+                  transition={{
+                    duration: 0.2,
+                    ease: 'easeInOut'
+                  }}
                 >
                   {t(`menu.${destination.label}`)}
                 </motion.span>
@@ -105,6 +124,10 @@ const NavigationRail = ({
                 className={styles['navigation-item-label']}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.2,
+                  ease: 'easeInOut'
+                }}
               >
                 {t(`menu.${destination.label}`)}
               </motion.span>

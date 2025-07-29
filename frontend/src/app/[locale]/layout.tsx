bae37aef0a4cb8ac3bccdd7dc722ca07991b0233
cherry <jms02140@gmail.com>
2025-07-29 15:23:43 +0900

    feat: 로그인 상태에 따라 settings/guest 버튼 조건부 렌더링 하도록 수정

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -2,22 +2,25 @@ import Navigation from '@/components/Navigation/Navigation';
 import I18nProvider from '@/providers/I18nProvider';
 import { StoreProvider } from '@/providers/StoreProvider';
 import ErrorBoundaryWrapper from '@/providers/ErrorBoundaryWrapper';
-import { QueryProvider } from '@/providers/QueryProvider';
 
 import styles from './layout.module.scss';
+import { AuthService } from '@/services/AuthService';
+import { CookieService } from '@/services/CookieService';
 
-const NavigationLayout = ({ children }: { children: React.ReactNode }) => {
+const NavigationLayout = async ({ children }: { children: React.ReactNode }) => {
+  const cookieService = new CookieService();
+  const authService = new AuthService(cookieService);
+  const isLoggedIn = await authService.isLoggedIn();
+
   return (
     <ErrorBoundaryWrapper>
       <StoreProvider>
-        <QueryProvider>
-          <div className={styles['navigation-layout']}>
-            <I18nProvider>
-              <Navigation />
-              {children}
-            </I18nProvider>
-          </div>
-        </QueryProvider>
+        <div className={styles['navigation-layout']}>
+          <I18nProvider>
+            <Navigation isLoggedIn={isLoggedIn} />
+            {children}
+          </I18nProvider>
+        </div>
       </StoreProvider>
     </ErrorBoundaryWrapper>
   );
